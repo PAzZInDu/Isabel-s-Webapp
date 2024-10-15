c472bc0990c85536a875984bc660f2f01a99a474
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2024-10-15 12:38:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def prediction(modelname, sample_image, IMG_SIZE=IMAGE_SIZE):
 #Building the website
 
 #title of the web page
-st.title("Alzheimer's Classification based on MRI Scan")
+st.title("Alzheimer's Classification Based On MRI Scan")
 
 #setting the main picture
 st.image(IMAGE_URL, caption = "Dementia")
