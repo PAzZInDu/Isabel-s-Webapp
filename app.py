f26c1f0e005705f3966890423f3669fbb1b4a738
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2024-10-15 12:37:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from keras.applications.vgg16 import preprocess_input
 from PIL import Image
 
 # Define Constants #
-IMG_SIZE = (224, 224)
+IMAGE_SIZE = (224, 224)
 LABELS = ['Mild_Demented', 'Moderate_Demented', 'Non_Demented', 'Very_Mild_Demented']
 MODEL_NAME = "best_model_vgg16.h5"
 IMAGE_URL = "https://www.healthshots.com/wp-content/uploads/2020/03/dementia-signs-.jpg"
 
 ##function#
 @st.cache_resource
-def prediction(modelname, sample_image, IMG_SIZE=IMG_SIZE):
+def prediction(modelname, sample_image, IMG_SIZE=IMAGE_SIZE):
 
     #sort the labels
     LABELS.sort()
@@ -48,7 +48,7 @@ st.header("About the Web App")
 #details about the project
 with st.expander("Web App 🌐"):
     st.subheader("Alzheimer's Classifictaion")
-    st.write("This web app can predict whether a given MRI Scan image shows signs of alzheimer's such as Mild_Demented, Moderate_Demented, Non_Demented, Very_Mild_Demented")
+    st.write("This web app can predict whether a given MRI Scan image shows signs of alzheimer's such as Mild Demented, Moderate Demented, Non Demented and Very Mild Demented")
 
 
 image =st.file_uploader("Upload a brain MRI scan image",type = ['jpg','png','jpeg'])
